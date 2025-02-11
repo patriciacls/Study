e375f9584c1af35be03edf70ba1a34e68f6c0fa2
Patricia Correia de Lima Santos <patriciacorreialimma@gmail.com>
2025-02-11 10:41:53 -0300

    20250211

---
+++ jvscrpt/boolean.js
@@ -0,0 +1,9 @@
+const estudante = 'Iuri'
+const aprovado = false
+
+if (aprovado === true) {  
+    console.log('Parabéns ' + estudante + ', você foi aprovado!')
+} else {console.log('Infelizmente ' + estudante + ', você foi reprovado!')}
+
+/*Use == quando você deseja comparar valores, permitindo a conversão de tipos. 
+Use === quando você deseja comparar valores e tipos, sem permitir a conversão de tipos.*/
---
+++ jvscrpt/typeStrings.js
@@ -0,0 +1,10 @@
+const cumprimento = ' "teste" ';
+const citacao = ` "teste" `;
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`O grande${citacao}`)
+
+const valida = ('deixa maiusculo' + citacao.toUpperCase())
+console.log(valida)
