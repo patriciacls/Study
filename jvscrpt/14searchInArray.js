a60f8cecf3c10915805bb40199f6f97697c70c9b
Patricia Correia de Lima Santos <patriciacorreialimma@gmail.com>
2025-02-25 11:50:53 -0300

    20250225

---
+++ jvscrpt/14searchInArray.js
@@ -0,0 +1,12 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+const lista = [alunos, medias];
+
+function exibirNomeNota(aluno) {
+    if (lista[0].includes(aluno)){
+        const indice = lista[0].indexOf(aluno) {
+            if (lista[0].includes(aluno))
+        }
+    }
+
+}
